2b02f8c6a4bea5f66fd5c968d1033854b62c58b3
pvsantos1 <140987738+pvsantos1@users.noreply.github.com>
2025-03-06 22:29:57 +0000

    Atualização

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,7 +11,8 @@ int main() {
     int escolha1, escolha2;
     float valor1_c1, valor1_c2, valor2_c1, valor2_c2, soma1, soma2;
     char *vencedor;
-    
+    char *atributo1_nome, *atributo2_nome;
+
     printf("Cadastro da primeira carta:\n");
     printf("Estado (A a H): ");
     scanf(" %c", &estado1);
@@ -49,9 +50,11 @@ int main() {
     
     printf("\nEscolha o primeiro atributo para comparar:\n");
     printf("1 - População\n2 - Área\n3 - PIB\n4 - Pontos turísticos\n5 - Densidade Demográfica (menor vence)\n");
+    printf("Sua opção: ");
     scanf("%d", &escolha1);
     
     printf("\nEscolha o segundo atributo (diferente do primeiro):\n");
+    printf("Sua segunda opção: ");
     scanf("%d", &escolha2);
     
     if (escolha2 == escolha1) {
@@ -60,19 +63,59 @@ int main() {
     }
     
     switch (escolha1) {
-        case 1: valor1_c1 = populacao1; valor1_c2 = populacao2; break;
-        case 2: valor1_c1 = area1; valor1_c2 = area2; break;
-        case 3: valor1_c1 = PIB1; valor1_c2 = PIB2; break;
-        case 4: valor1_c1 = pontos_turisticos1; valor1_c2 = pontos_turisticos2; break;
-        case 5: valor1_c1 = densidade1; valor1_c2 = densidade2; break;
+        case 1: 
+            valor1_c1 = populacao1;
+            valor1_c2 = populacao2;
+            atributo1_nome = "População";
+            break;
+        case 2: 
+            valor1_c1 = area1;
+            valor1_c2 = area2;
+            atributo1_nome = "Área";
+            break;
+        case 3: 
+            valor1_c1 = PIB1;
+            valor1_c2 = PIB2;
+            atributo1_nome = "PIB";
+            break;
+        case 4: 
+            valor1_c1 = pontos_turisticos1;
+            valor1_c2 = pontos_turisticos2;
+            atributo1_nome = "Pontos Turísticos";
+            break;
+        case 5: 
+            valor1_c1 = densidade1;
+            valor1_c2 = densidade2;
+            atributo1_nome = "Densidade Demográfica";
+            break;
     }
     
     switch (escolha2) {
-        case 1: valor2_c1 = populacao1; valor2_c2 = populacao2; break;
-        case 2: valor2_c1 = area1; valor2_c2 = area2; break;
-        case 3: valor2_c1 = PIB1; valor2_c2 = PIB2; break;
-        case 4: valor2_c1 = pontos_turisticos1; valor2_c2 = pontos_turisticos2; break;
-        case 5: valor2_c1 = densidade1; valor2_c2 = densidade2; break;
+        case 1: 
+            valor2_c1 = populacao1;
+            valor2_c2 = populacao2;
+            atributo2_nome = "População";
+            break;
+        case 2: 
+            valor2_c1 = area1;
+            valor2_c2 = area2;
+            atributo2_nome = "Área";
+            break;
+        case 3: 
+            valor2_c1 = PIB1;
+            valor2_c2 = PIB2;
+            atributo2_nome = "PIB";
+        break;
+        case 4: 
+            valor2_c1 = pontos_turisticos1;
+            valor2_c2 = pontos_turisticos2;
+            atributo2_nome = "Pontos Turísticos";
+            break;
+        case 5: 
+            valor2_c1 = densidade1;
+            valor2_c2 = densidade2;
+            atributo2_nome = "Densidade Demográfica";
+            break;
     }
     
     soma1 = valor1_c1 + valor2_c1;
@@ -82,8 +125,11 @@ int main() {
     
     printf("\n--- Resultado da Comparação ---\n");
     printf("%s vs %s\n", nome_cidade1, nome_cidade2);
-    printf("%s: %.2f + %.2f = %.2f\n", nome_cidade1, valor1_c1, valor2_c1, soma1);
-    printf("%s: %.2f + %.2f = %.2f\n", nome_cidade2, valor1_c2, valor2_c2, soma2);
+    printf("Atributo 1: %s\n%s: %.2f\n%s: %.2f\n\n", atributo1_nome, nome_cidade1, valor1_c1, nome_cidade2, valor1_c2);
+    printf("Atributo 2: %s\n%s: %.2f\n%s: %.2f\n\n", atributo2_nome, nome_cidade1, valor2_c1, nome_cidade2, valor2_c2);
+    printf("Soma total dos atributos:\n");
+    printf("%s: %.2f\n", nome_cidade1, soma1);
+    printf("%s: %.2f\n", nome_cidade2, soma2);
     printf("Vencedor: %s\n", vencedor);
     
     return 0;
